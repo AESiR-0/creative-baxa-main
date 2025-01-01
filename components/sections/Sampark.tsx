94d54053817cb95e1775c3d85917b3978cf7b5f4
AESiR <cyborgkiller1008@gmail.com>
2025-01-01 09:17:02 +0530

    Refactor Sampark component to enhance form functionality and validation; update Testimonials section with new image asset and styling improvements.

--- components/sections/Sampark.tsx
+++ components/sections/Sampark.tsx
@@ -1,91 +1,238 @@
 "use client";
-
 import React, { useState } from "react";
 
-export default function Sampark() {
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [focused, setFocused] = useState(false);
-    const [selected, setSelected] = useState('');
-    const [formData, setFormData] = useState({
-        interestedIn: "",
-        contact: ''
-    })
-    return (
-        <div className="flex relative px-10 flex-col items-start gap-16 justify-center min-h-screen bg-secondary text-white p-6">
-            <div className="text-6xl w-full max-md:text-3xl max-md:mb-5  max-md:items-start max-md:justify-start max-md:m-0 max-md:p-0  font-bold md:mb-6  md:pr-32">
-                Kya Hum Iss
-                <span className="text-[#80D3FF] "> Project idea</span>{" "}
-                <span>ko <br /> haqeeqat mein badal de?</span>
-            </div>
-            <div className="relative w-96">
+import { useRouter } from "next/navigation";
+type FocusedFields = {
+  [key: string]: boolean;
+};
+
+type FormData = {
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  experience: number;
+  jobTitle: string | null;
+  aboutExperience: string;
+  cv: string;
+  playlist: string;
+  favComic: string;
+};
+const formFields = [
+  { type: "text", name: "fullName", label: "Full Name" },
+  { type: "email", name: "email", label: "Email" },
+  { type: "tel", name: "phoneNumber", label: "Phone Number" },
+  { type: "text", name: "project_company_name", label: "Project/Company Name" },
+];
+
+const Form: React.FC = () => {
+  const Router = useRouter();
+
+  const [focused, setFocused] = useState<FocusedFields>({});
+  const [selected, setSelected] = useState<string | null>(null);
+  const [fieldsRequired, setFieldsRequired] = useState<boolean>(false);
+  const [formData, setFormData] = useState<FormData>({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    experience: 0,
+    jobTitle: null,
+    aboutExperience: "",
+    cv: "",
+    playlist: "",
+    favComic: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleFocus = (field: string) => {
+    setFocused({ ...focused, [field]: true });
+  };
+
+  const handleBlur = (field: string, value: string) => {
+    if (!value) {
+      setFocused({ ...focused, [field]: false });
+    }
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    console.log(1);
+    setFieldsRequired(false);
+    const {
+      fullName,
+      email,
+      phoneNumber,
+      experience,
+      jobTitle,
+      aboutExperience,
+      cv,
+      favComic,
+      playlist,
+    } = formData;
+    if (
+      !fullName ||
+      !email ||
+      !phoneNumber ||
+      !experience ||
+      !jobTitle ||
+      jobTitle === "" ||
+      jobTitle === " " ||
+      !aboutExperience ||
+      !cv
+    ) {
+      alert("Please fill all the fields required");
+      setFieldsRequired(true);
+      setIsSubmitting(false);
+
+      return;
+    }
+
+    const dataToSend = {
+      fullName,
+      email,
+      phoneNumber,
+      experience,
+      jobTitle,
+      aboutExperience,
+      cv,
+      favComic,
+      playlist,
+    };
+
+    if (!fieldsRequired) {
+      try {
+        const response = await fetch("/api/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataToSend),
+        });
+
+        if (response.ok) {
+          setIsModalOpen(true); // Open modal upon success
+          setIsSubmitting(false);
+          setFormData({
+            fullName: "",
+            email: "",
+            phoneNumber: "",
+            experience: 0,
+            jobTitle: null,
+            aboutExperience: "",
+            cv: "",
+            playlist: "",
+            favComic: "",
+          });
+        } else {
+          setIsModalOpen(true);
+          alert("Failed to submit form. Please try again.");
+          setIsSubmitting(false);
+        }
+      } catch (error) {
+        console.error("Error submitting form:", error);
+        alert("Error submitting form. Please try again.");
+        setIsSubmitting(false);
+      }
+    }
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    Router.push("https://youtu.be/xvFZjo5PgG0?si=h9qItXjsVyKBb6fK");
+  };
+
+  return (
+    <div className="min-h-screen transition-colors  bg-[#0E0D0D] flex items-center justify-center text-white px-4">
+      <div className="w-full max-w-4xl p-8 rounded-lg shadow-lg">
+        <div className="text-5xl w-full max-md:text-3xl max-md:mb-5  max-md:items-start max-md:justify-start max-md:m-0 max-md:p-0  font-bold md:mb-6 md:pr-32">
+          <span>Kya Hum Iss </span>
+          <span className="text-[#80D3FF] "> Project idea </span>{" "}
+          <span>ko haqeeqat mein badal de?</span>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-x-10 gap-y-4 py-5 grid-cols-2 md:grid-cols-2">
+            {formFields.map((field, index) => (
+              <div key={index} className="relative mb-6">
                 <input
-                    required
-                    disabled={isSubmitting}
-                    type='text'
-                    name='contact'
-                    value={formData.contact}
-                    onFocus={() => setFocused(false)}
-                    onBlur={(e) => setFocused(true)}
-                    onChange={(e) => {
-                        setFormData((prev) => ({
-                            ...prev,
-                            contact: e.target.value,
-                        }));
-                    }}
-                    className="w-full disabled:text-[#80D3FF] bg-secondary rounded-none text-white py-3 px-2 outline-none border border-t-0 border-l-0 border-r-0 border-[#80D3FF] transition"
+                  required
+                  disabled={isSubmitting}
+                  type={field.type}
+                  name={field.name}
+                  value={formData[field.name as keyof FormData] || ""}
+                  onFocus={() => handleFocus(field.name)}
+                  onBlur={(e) => handleBlur(field.name, e.target.value)}
+                  onChange={handleChange}
+                  className="w-full disabled:text-[#80D3FF]  bg-[#0E0D0D] rounded-none text-white py-3 px-2 outline-none border border-t-0 border-l-0 border-r-0 border-[#80D3FF] transition"
                 />
                 <label
-                    className={`absolute left-1 top-1/2 transform -translate-y-1/2 text-white text-xl pointer-events-none transition-all duration-200 ${!focused
-                        ? "-translate-y-10 left-0 text-sm text-[#80D3FF]"
-                        : ""
-                        }`}
+                  className={`absolute left-1 top-1/2 transform -translate-y-1/2 text-gray-400 pointer-events-none transition-all duration-200 ${
+                    focused[field.name]
+                      ? "-translate-y-10 left-0 text-sm text-[#80D3FF]"
+                      : ""
+                  }`}
                 >
-                    Email or Phone Number*
+                  {field.label}*
                 </label>
-            </div>
-            <div className="">
-                <p className="text-3xl mb-16">I'm Interested in:</p>
+              </div>
+            ))}
+          </div>
 
-                <div className="grid grid-cols-4 md:grid max-md:grid-cols-2 justify-between gap-5 mb-6">
-                    {[
-                        "Events",
-                        "Content Creation",
-                        "Website Development",
-                        "Other",
-                    ].map((option) => (
-                        <button
-                            key={option}
-                            type="button"
-                            disabled={isSubmitting}
-                            onClick={() => {
-                                // Toggle the jobTitle in the form data and set the selected option
-                                const newJobTitle = selected === option ? null : option;
-                                setSelected(newJobTitle || '');
-                                setFormData((prev) => ({
-                                    ...prev,
-                                    jobTitle: newJobTitle, // Update jobTitle directly based on new selected option
-                                }));
-                            }}
-                            className={` hover:bg-[#80D3FF] ${formData.interestedIn === option
-                                ? "disabled:bg-[#c8e5f5] bg-[#80D3FF] border border-[#80D3FF]"
-                                : "border border-white bg-transparent "
-                                } py-2 px-2 rounded-3xl transition`}
-                        >
-                            {option}
-                        </button>
-                    ))}
-                </div>
+          <div className="mt-5">
+            <h3 className="text-3xl py-5">
+              I<span className="text-primary">'</span>m Interested in:
+            </h3>
+            <div className="grid grid-cols-4 md:grid max-md:grid-cols-2 justify-between gap-10 mb-6">
+              {[
+                "Events",
+                "Website Development",
+                "Content Creation",
+                "Other",
+              ].map((option) => (
+                <button
+                  key={option}
+                  type="button"
+                  disabled={isSubmitting}
+                  onClick={() => {
+                    const newJobTitle = selected === option ? null : option;
+                    setSelected(newJobTitle);
+                    setFormData((prev) => ({
+                      ...prev,
+                      jobTitle: newJobTitle, // Update jobTitle directly based on new selected option
+                    }));
+                  }}
+                  className={` hover:bg-[#80D3FF] ${
+                    formData.jobTitle === option
+                      ? "disabled:bg-[#c8e5f5] bg-[#80D3FF] border border-[#80D3FF]"
+                      : "border border-white bg-transparent "
+                  } py-2 px-2 rounded-3xl transition`}
+                >
+                  {option}
+                </button>
+              ))}
             </div>
-            <button
-                type="submit"
-                disabled={isSubmitting}
-                className="w-40 text-xl flex justify-center items-center transition-all  disabled:bg-[#d0e9f7] disabled:opacity-75 hover:border-white hover:border hover:bg-transparent bg-[#80D3FF] py-3 rounded-xl text-white font-semibold hover:bg-[#80D3FF] "
-            >
-                Submit
-                {isSubmitting && (
-                    <span className="animate-spin inline-block w-5 h-5  border-4 border-t-transparent border-white rounded-full"></span>
-                )}
-            </button>
-        </div>
-    );
-}
+          </div>
+
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="w-full flex justify-center items-center transition-all gap-5 disabled:bg-[#d0e9f7] disabled:opacity-75 hover:border-white hover:border hover:bg-transparent bg-[#80D3FF] py-3 rounded-xl text-white font-semibold hover:bg-[#80D3FF] "
+          >
+            SUBMIT{" "}
+            {isSubmitting && (
+              <span className="animate-spin inline-block w-5 h-5  border-4 border-t-transparent border-white rounded-full"></span>
+            )}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Image from 'next/image';
+import callOut from '@/public/static/section/testimonial/testimonial.png'
 export default function Testimonials() {
     const testimonials = [
         {
@@ -35,22 +36,19 @@ export default function Testimonials() {
     return (
         <div className="bg-gray-100 py-10 px-5">
             <h1 className="text-center text-2xl font-bold mb-8">Not Paid.</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
+            <div className="grid grid-cols-1 gap-20 sm:grid-cols-2 lg:grid-cols-3 justify-center">
                 {testimonials.map((testimonial, index) => (
                     <div
                         key={index}
-                        className="relative p-6 bg-white border border-gray-300 rounded-lg shadow-md"
+                        className="relative p-6  border border-gray-800 rounded-lg shadow-md"
                     >
-                        <svg
+                        <Image
+                            src={callOut}
+                            width={256}
+                            height={256}
+                            alt="Testimonial Call out"
                             className="absolute -top-5 left-1/2 transform -translate-x-1/2"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="50"
-                            height="50"
-                            viewBox="0 0 430 286"
-                            fill="currentColor"
-                        >
-                            <path d="M397.5 5.10623H31.8136C17.0774 5.10623 5.13123 16.9942 5.13123 31.6587V195.302C5.13123 207.71 15.2395 217.769 27.7087 217.769H255.536C265.168 217.769 273.988 223.137 278.376 231.67L303.114 279.779C303.997 281.498 306.563 281.15 306.952 279.258L314.557 242.266C317.488 228.007 330.095 217.769 344.719 217.769H397.5C412.236 217.769 424.182 205.881 424.182 191.217V31.6586C424.182 16.9942 412.236 5.10623 397.5 5.10623ZM31.8136 0C14.2435 0 0 14.1741 0 31.6587V195.302C0 210.53 12.4056 222.875 27.7087 222.875H255.536C263.242 222.875 270.298 227.17 273.808 233.996L298.546 282.105C301.639 288.121 310.618 286.902 311.979 280.281L319.584 243.289C322.027 231.407 332.532 222.875 344.719 222.875H397.5C415.07 222.875 429.314 208.701 429.314 191.217V31.6586C429.314 14.1741 415.07 0 397.5 0H31.8136Z" />
-                        </svg>
+                        />
                         <h3 className="text-lg font-semibold">{testimonial.name}</h3>
                         <p className="text-sm text-gray-600 mt-2">{testimonial.text}</p>
                     </div>
